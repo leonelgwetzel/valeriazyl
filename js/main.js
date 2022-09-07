f466a554936ea9ff55292b56b1ecf183883e1ed4
leonelgwetzel <wetzel.leonel.gabriel@gmail.com>
2022-09-07 17:05:47 -0300

    Index 95%

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
 if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 
-document.getElementById("navbar").style.background = "rgb(237, 106, 90)";
+document.getElementById("navbar").style.background = "rgb(153,133,110)";
 } else {
 
-document.getElementById("navbar").style.background = "none";
+document.getElementById("navbar").style.background = "rgba(153,133,110,0.3)";
 }
 }
