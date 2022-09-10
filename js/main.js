55d3b935280e9ffca1adcf9406d5283b9afe4b7b
leonelgwetzel <wetzel.leonel.gabriel@gmail.com>
2022-09-10 20:12:49 -0300

    Index finalizado, terminando about

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
 if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 
-document.getElementById("navbar").style.background = "rgb(153,133,110)";
+document.getElementById("navbar").style.background = "rgb(191, 95, 84)";
 } else {
 
-document.getElementById("navbar").style.background = "rgba(153,133,110,0.3)";
+document.getElementById("navbar").style.background = "rgba(191, 95, 84, 0.3)";
 }
 }
