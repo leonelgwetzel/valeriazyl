6347f6421250b1a9f2f22739914baba7698de3cd
leonelgwetzel <wetzel.leonel.gabriel@gmail.com>
2022-09-19 16:09:57 -0300

    Masajes y Retiros

---
+++ js/cd.js
@@ -0,0 +1,25 @@
+function updateTimer() {
+    future  = new Date('12/03/2022');
+    now     = new Date();
+    diff    = future - now;
+  
+    days  = Math.floor( diff / (1000*60*60*24) );
+    hours = Math.floor( diff / (1000*60*60) );
+    mins  = Math.floor( diff / (1000*60) );
+    secs  = Math.floor( diff / 1000 );
+  
+    d = days;
+    h = hours - days  * 24;
+    m = mins  - hours * 60;
+    s = secs  - mins  * 60;
+  
+    document.getElementById("timer")
+      .innerHTML =
+        '<div>' + d + '<span>Días</span></div>' +
+        '<div>' + h + '<span>Horas</span></div>' +
+        '<div>' + m + '<span>Minutos</span></div>' +
+        '<div>' + s + '<span>Segundos</span></div>' ;
+
+  }
+  setInterval('updateTimer()', 1000 );
+  
