39a55451ded296773bef221e6862702a08cb6286
leonelgwetzel <wetzel.leonel.gabriel@gmail.com>
2022-09-27 21:21:41 -0300

    finish02

--- js/main.js
+++ js/main.js
@@ -2,10 +2,15 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
 if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-
+    $("#bannerRetiro").removeClass("d-none");
 document.getElementById("navbar").style.background = "rgb(191, 95, 84)";
 } else {
 
 document.getElementById("navbar").style.background = "rgba(191, 95, 84, 0.3)";
 }
 }
+
+//Cerrar Banner
+$("#closeBanner").click(function(){
+    $("#bannerRetiro").remove();
+});
